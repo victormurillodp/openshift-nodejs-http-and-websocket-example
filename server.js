b862a7233d083b7606ba0b3502636461704c58cb
Corey Daley <cdaley@redhat.com>
2013-10-24 11:59:04 -0400

    updating

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
-var port      = process.env.OPENSHIFT_NODEJS_PORT || 8000;
+var port      = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 
 var WebSocketServer = require('ws').Server
   , wss = new WebSocketServer({host : ipaddress, port: port});
