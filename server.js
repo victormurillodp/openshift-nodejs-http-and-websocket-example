6089af1493171fc0a39b34629d27bfc1b04583f2
Corey Daley <cdaley@redhat.com>
2013-10-24 11:48:04 -0400

    updating

--- server.js
+++ server.js
@@ -1,9 +1,37 @@
 #!/bin/env node
-var io = require('socket.io').listen(8080);
+// socket.io initialization on the server side
+self.initializeSocketIO = function() {
+        self.server = require('http').createServer(self.app);
+        self.io = require('socket.io').listen(self.server);
+        self.io.enable('browser client minification');  // send minified client
+        self.io.enable('browser client etag');          // apply etag caching logic based on version number
+        self.io.enable('browser client gzip');          // gzip the file
+        self.io.set('log level', 1);                    // reduce logging
 
-io.sockets.on('connection', function (socket) {
-  socket.emit('news', { hello: 'world' });
-  socket.on('my other event', function (data) {
-    console.log(data);
-  });
-});
+        self.io.set('transports', [
+                'websocket'
+            ]);
+        return this;
+    }
+
+    self.addSocketIOEvents = function() {
+        self.io.sockets.on('connection', function (socket) {
+          socket.emit('news', { hello: 'world' });
+          socket.on('my other event', function (data) {
+            console.log(data);
+      });
+    });
+}
+
+/**
+ *  Initializes the sample application.
+ */
+self.initialize = function() {
+    self.setupVariables();
+    self.populateCache();
+    self.setupTerminationHandlers();
+
+    // Create the express server and routes.
+    self.initializeServer();
+    self.initializeSocketIO().addSocketIOEvents();
+};
