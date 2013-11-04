9ddca37f5646ba1f2cb8a7bab66c6eaf6c956114
Corey Daley <cdaley@redhat.com>
2013-11-04 01:06:14 -0500

    updating

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var server = http.createServer(function(request, response) {
     console.log((new Date()) + ' Received request for ' + request.url);
 	response.writeHead(200, {'Content-Type': 'text/plain'});
 	  response.write("Welcome to Node.js on OpenShift!\n\n");
-	  response.end("Your IP address seems to be \n");
+	  response.end("Thanks for visiting us! \n");
 });
 
 server.listen( port, ipaddress, function() {
